c4c056428a87cc382937da135222b93059b5880f
Bryan Jarvis <pootwaddle@pootwaddle.com>
2020-11-03 08:00:15 -0600

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module pootwaddle.com/schedule
+
+go 1.15
+
+require github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
---
+++ schedule.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	//"time"
+
+	"github.com/carlescere/scheduler"
+)
+
+func main() {
+
+	fortune := func() {
+		fmt.Println("scheduleFortune")
+		cmd := exec.Command("CMD", "/C C:\\AUTOJOB\\FORTUN.BAT")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("whoops!", err)
+		}
+	}
+
+	malware1 := func() {
+		fmt.Println("scheduleMalwareBytes")
+		cmd := exec.Command("CMD", "/C C:\\AUTOJOB\\mw1.bat")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("whoops!", err)
+		}
+	}
+
+	reserves := func() {
+		fmt.Println("scheduleReserves")
+		cmd := exec.Command("CMD", "/C C:\\AUTOJOB\\RESERVES.BAT")
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("whoops!", err)
+		}
+	}
+
+	//scheduler.Every(2).Seconds().NotImmediately().Run(job1)
+	scheduler.Every().Day().At("05:00").Run(fortune)
+	scheduler.Every().Day().At("02:20").Run(malware1)
+	scheduler.Every().Monday().At("06:20").Run(reserves)
+
+	// Keep the program from exiting.
+	runtime.Goexit()
+}
