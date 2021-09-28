d990a2c726e82775e398122619e4aa206f042aa0
Bryan Jarvis <pootwaddle@pootwaddle.com>
2021-09-27 21:22:26 -0500

    add some logging to a file

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module pootwaddle.com/schedule
 
 go 1.15
 
-require github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
+require (
+	github.com/carlescere/scheduler v0.0.0-20170109141437-ee74d2f83d82
+	github.com/romana/rlog v0.0.0-20171115192701-f018bc92e7d7
+	github.com/stretchr/testify v1.7.0 // indirect
+)
--- schedule.go
+++ schedule.go
@@ -4,56 +4,75 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"time"
 
-	//"time"
-
 	"github.com/carlescere/scheduler"
+	"github.com/romana/rlog"
 )
 
 func main() {
 
+	//initialize logging
+	logfileName := fmt.Sprintf("schedule_%s", time.Now().Format("20060102"))
+	// Example of redirecting log output to a new file at runtime
+	newLogFile, err := os.OpenFile(filepath.Join("D:\\ARCHIVE\\"+logfileName+".log"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+	if err == nil {
+		rlog.SetOutput(newLogFile)
+		rlog.Info(os.Args[0] + " started")
+	}
+
 	fortune := func() {
-		fmt.Println("scheduleFortune")
-		cmd := exec.Command("CMD", "/C C:\\AUTOJOB\\FORTUN.BAT")
+		rlog.Info("scheduleFortune")
+		cmd := exec.Command("CMD", "/C D:\\ARCHIVE\\FORTUN.BAT")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		err := cmd.Run()
 		if err != nil {
-			fmt.Println("whoops!", err)
+			rlog.Error("scheduleFortune failed: ", err)
 		}
-		time.Sleep(3 * time.Second)
 	}
 
 	malware1 := func() {
-		fmt.Println("scheduleMalwareBytes")
-		cmd := exec.Command("CMD", "/C C:\\AUTOJOB\\mw1.bat")
+		rlog.Info("scheduleMalwareBytes")
+		cmd := exec.Command("CMD", "/C D:\\ARCHIVE\\mw1.bat")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		err := cmd.Run()
 		if err != nil {
-			fmt.Println("whoops!", err)
+			rlog.Error("scheduleMalwareBytes failed:", err)
 		}
-		time.Sleep(3 * time.Second)
 	}
 
 	reserves := func() {
-		fmt.Println("scheduleReserves")
-		cmd := exec.Command("CMD", "/C C:\\AUTOJOB\\RESERVES.BAT")
+		rlog.Info("scheduleReserves")
+		cmd := exec.Command("CMD", "/C D:\\ARCHIVE\\RESERVES.BAT")
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 		err := cmd.Run()
 		if err != nil {
-			fmt.Println("whoops!", err)
+			rlog.Error("scheduleReserves failed: ", err)
 		}
-		time.Sleep(3 * time.Second)
+	}
+
+	rotateLog := func() {
+		rlog.Info("rotateLog")
+		//initialize new log file
+		logfileName := fmt.Sprintf("cert-check_%s", time.Now().Format("20060102"))
+		// Example of redirecting log output to a new file at runtime
+		newLogFile, err := os.OpenFile(filepath.Join("D:\\ARCHIVE\\"+logfileName+".log"), os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0666)
+		if err == nil {
+			rlog.SetOutput(newLogFile)
+			rlog.Info(newLogFile)
+		}
 	}
 
 	//scheduler.Every(2).Seconds().NotImmediately().Run(job1)
 	scheduler.Every().Day().At("05:00:15").Run(fortune)
 	scheduler.Every().Day().At("06:35:15").Run(malware1)
 	scheduler.Every().Monday().At("06:20:15").Run(reserves)
+	scheduler.Every().Day().At("00:00:01").Run(rotateLog)
 
 	// Keep the program from exiting.
 	runtime.Goexit()
